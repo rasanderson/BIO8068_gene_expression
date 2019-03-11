ce271bd0814b31f1e29cd0c20dc47a19e920742c
rasanderson <roy.sanderson@newcastle.ac.uk>
2019-03-11 22:04:16 +0000

    Replaced older mutate_each with newer mutate_at function. Included data in repository.

--- gene_expression.R
+++ gene_expression.R
@@ -10,6 +10,34 @@ View(original_data)
 # First entries of NAME
 original_data$NAME[1:3]
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
+#
 # Load essential libraries
 library(dplyr)
 library(tidyr)
@@ -20,36 +48,190 @@ library(tidyr)
 cleaned_data <- original_data %>%
   separate(NAME, c("name", "BP", "MF", "systematic_name", "number"), sep = "\\|\\|")
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
 # Separate NAME, cleanup whitespace
 cleaned_data <- original_data %>%
   separate(NAME, c("name", "BP", "MF", "systematic_name", "number"), sep = "\\|\\|") %>%
-  mutate_each(funs(trimws), name:systematic_name)
+  mutate_at(vars(name, BP, systematic_name, MF), funs(trimws(.)))
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
+#
 # Separate NAME, cleanup whitespace and drop columns
 cleaned_data <- original_data %>%
   separate(NAME, c("name", "BP", "MF", "systematic_name", "number"), sep = "\\|\\|") %>%
-  mutate_each(funs(trimws), name:systematic_name) %>%
+  mutate_at(vars(name, BP, systematic_name, MF), funs(trimws(.))) %>% 
   select(-number, -GID, -YORF, -GWEIGHT)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
 # Separate NAME, cleanup whitespace, drop columns and gather
 cleaned_data <- original_data %>%
   separate(NAME, c("name", "BP", "MF", "systematic_name", "number"), sep = "\\|\\|") %>%
-  mutate_each(funs(trimws), name:systematic_name) %>%
+  mutate_at(vars(name, BP, systematic_name, MF), funs(trimws(.))) %>% 
   select(-number, -GID, -YORF, -GWEIGHT) %>%
   gather(sample, expression, G0.05:U0.3)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
+#
 library(stringr)
 
 # Separate NAME, cleanup whitespace, drop columns, gather and separate nutrient from rate
 cleaned_data <- original_data %>%
   separate(NAME, c("name", "BP", "MF", "systematic_name", "number"), sep = "\\|\\|") %>%
-  mutate_each(funs(trimws), name:systematic_name) %>%
+  mutate_at(vars(name, BP, systematic_name, MF), funs(trimws(.))) %>% 
   select(-number, -GID, -YORF, -GWEIGHT) %>%
   gather(sample, expression, G0.05:U0.3) %>%
   separate(sample, c("nutrient", "rate"), sep = 1, convert = TRUE)
 
 
-# Data now tidy; start to explore ####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
+#
+#
+# At last! Data now tidy; start to explore ####
 library(ggplot2)
 
 # ggplot of leucine
@@ -58,6 +240,39 @@ cleaned_data %>%
   ggplot(aes(rate, expression, color = nutrient)) +
   geom_line()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
 # All the leucine BP
 cleaned_data %>%
   filter(BP == "leucine biosynthesis") %>%
@@ -65,6 +280,39 @@ cleaned_data %>%
   geom_line() +
   facet_wrap(~name)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
+#
 # All the leucine BP plus linear model
 cleaned_data %>%
   filter(BP == "leucine biosynthesis") %>%
@@ -73,6 +321,40 @@ cleaned_data %>%
   geom_smooth(method = "lm", se = FALSE) +
   facet_wrap(~name)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
+#
+#
 # Ditto for Sulphur metabolism
 # Sulphur metabolism
 cleaned_data %>%
@@ -82,6 +364,41 @@ cleaned_data %>%
   geom_smooth(method = "lm", se = FALSE) +
   facet_wrap(~name + systematic_name, scales = "free_y")
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
+#
+#
 # Take home message: not much code needed ####
 # # Core code
 # library(dplyr)
@@ -90,7 +407,7 @@ cleaned_data %>%
 # 
 # cleaned_data <- original_data %>%
 #   separate(NAME, c("name", "BP", "MF", "systematic_name", "number"), sep = "\\|\\|") %>%
-#   mutate_each(funs(trimws), name:systematic_name) %>%
+#   mutate_at(vars(name, BP, systematic_name, MF), funs(trimws(.))) %>% 
 #   select(-number, -GID, -YORF, -GWEIGHT) %>%
 #   gather(sample, expression, G0.05:U0.3) %>%
 #   separate(sample, c("nutrient", "rate"), sep = 1, convert = TRUE)
@@ -101,3 +418,5 @@ cleaned_data %>%
 #   geom_point() +
 #   geom_smooth(method = "lm", se = FALSE) +
 #   facet_wrap(~name)
+#
+#
